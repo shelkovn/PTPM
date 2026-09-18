60beb8f376341cb45c9dd518fef10dd94c98b119
towa <..>
2026-09-18 16:14:37 +0700

    3 basic tests

--- tests/test_my_project.py
+++ tests/test_my_project.py
@@ -7,3 +7,19 @@ class test_triangles_app(unittest.TestCase):
         result = calculate_triangle("3", "4", "5")
         expected = ("разносторонний", [(12, 0), (88, 0), (88, 100)])
         self.assertEqual(result, expected)
+
+    def test_invalid_lengths(self):
+        result = calculate_triangle("10", "1", "1")
+        expected = ("не треугольник", [(-1, -1)] * 3)
+        self.assertEqual(result, expected)
+
+    #def test_exception(self):
+    #    with self.assertRaises(ValueError):
+    #        calculate_triangle("abc", "4", "5")
+
+    def test_invalid_values(self):
+        result = calculate_triangle("abc", "4", "5")
+        expected = ("", [(-2, -2)] * 3)
+        self.assertEqual(result, expected)
+
+    
